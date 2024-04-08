15aedaa99d3f267a8670c823fb7974d56e1d1fd4
엔터프라이즈IT개발팀/차선호 <tjsgh1512@naver.com>
2024-04-08 16:26:19 +0900

    filterchain 추가 및 분기

--- src/main/java/com/theono/authorization/config/SecurityConfig.java
+++ src/main/java/com/theono/authorization/config/SecurityConfig.java
@@ -1,11 +1,37 @@
 package com.theono.authorization.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain filterChain1(HttpSecurity http) throws Exception{
+
+        http
+                .securityMatchers((auth) -> auth.requestMatchers("/user"))
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/user").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain2(HttpSecurity http) throws Exception{
+
+        http
+                .securityMatchers((auth) -> auth.requestMatchers("/admin"))
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/admin").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
 
 }
---
+++ src/main/java/com/theono/authorization/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.theono.authorization.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class MainController {
+
+    @GetMapping("/user")
+    public String userPage(){
+        return "user page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage(){
+        return "admin page";
+    }
+
+}
