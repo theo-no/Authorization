2f302b57c22cd453b91556a651a15c8f5ede43eb
엔터프라이즈IT개발팀/차선호 <tjsgh1512@naver.com>
2024-04-08 16:32:16 +0900

    webSecurityCustomizer에 ignore할 requeset 추가

--- src/main/java/com/theono/authorization/config/SecurityConfig.java
+++ src/main/java/com/theono/authorization/config/SecurityConfig.java
@@ -3,13 +3,19 @@ package com.theono.authorization.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+
+    /**
+     * user 관련 필터
+     */
     @Bean
     public SecurityFilterChain filterChain1(HttpSecurity http) throws Exception{
 
@@ -22,6 +28,9 @@ public class SecurityConfig {
         return http.build();
     }
 
+    /**
+     * admin 관련 필터
+     */
     @Bean
     public SecurityFilterChain filterChain2(HttpSecurity http) throws Exception{
 
@@ -32,6 +41,14 @@ public class SecurityConfig {
                         .anyRequest().authenticated());
 
         return http.build();
+    }
+
+    /**
+     * 필터 거칠 필요 없는 요청
+     */
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers("*/img/**");
     }
 
 }
