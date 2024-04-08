d06044fd67dbc440f406deae515f2b9886fd7abc
엔터프라이즈IT개발팀/차선호 <tjsgh1512@naver.com>
2024-04-08 16:01:20 +0900

    add security dependency

---
+++ src/main/java/com/theono/authorization/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.theono.authorization.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+}
