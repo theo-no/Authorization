614f3f77e216ef8ba7c00a685e6d1daf4d34ba99
엔터프라이즈IT개발팀/차선호 <tjsgh1512@naver.com>
2024-04-12 09:46:44 +0900

    fix error

--- src/main/java/com/theono/authorization/controller/UserController.java
+++ src/main/java/com/theono/authorization/controller/UserController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @Controller
-@ResponseBody
-public class MainController {
-
-    @GetMapping
-    public String indexPage(){ return "index page"; }
-
-
+public class UserController {
+    @GetMapping("/user")
+    @ResponseBody
+    public String loginPage(){
+        return "welcome user page";
+    }
 }
